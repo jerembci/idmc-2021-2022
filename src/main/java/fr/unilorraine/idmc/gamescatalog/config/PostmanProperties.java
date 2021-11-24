5ca53736dd9d95da38d51fd0aac33e0bb7d48dfe
jerembci <jerembci@hotmail.fr>
2021-11-24 14:31:34 +0100

    PostmanProperties & index.html

---
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/config/PostmanProperties.java
@@ -0,0 +1,15 @@
+package fr.unilorraine.idmc.gamescatalog.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("app.postman")
+@Getter
+@Setter
+public class PostmanProperties {
+
+    private String echoUrl;
+}
